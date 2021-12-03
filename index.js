fd14eec97cea4ac4cd8a980dca6247bf513630df
dszhusb <71653499+dszhusb@users.noreply.github.com>
2021-12-03 17:03:38 -0500

    Removed Test files

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ database.loadDatabase();
 
 fetchTrendsData(['apple pie', 'peanut butter crepe', 'pineapple pizza', 'apple cider']);
 
-// app.get('/api', (request, response) => {
-//   database.fin({}, (err, data) => {
-//     if (err) {
-//       response.end();
-//       return;
-//     }
-//     response.json(data);
-//   })
-//   response.json({
-//     test: 123
-//   });
-// });
-
 function fetchTrendsData(foodArray) {
   for (let food of foodArray) {
     googleTrends.interestOverTime({
